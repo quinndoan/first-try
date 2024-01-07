1ca2733bcc62c70566e3c977e3420fb2a418fda5
Nguyễn Mỹ Duyên <myduyencocsau@gmail.com>
2024-01-07 11:48:47 +0700

    Update manageuser.php

--- Online-Shopping-System-With-Server-main/admin/admin/manageuser.php
+++ Online-Shopping-System-With-Server-main/admin/admin/manageuser.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-include("db.php");
+include("Online-Shopping-System-With-Server-main\admin\admin\includes\db.php");
 
 if (isset($_GET['action']) && $_GET['action'] != "" && $_GET['action'] == 'delete') {
     $user_id = $_GET['user_id'];
