8a0d72ef9543e8d02e5e7d1886f2d6c1ef4ae059
Nguyễn Mỹ Duyên <myduyencocsau@gmail.com>
2024-01-07 11:41:31 +0700

    Update addsuppliers.php

--- Online-Shopping-System-With-Server-main/admin/admin/addsuppliers.php
+++ Online-Shopping-System-With-Server-main/admin/admin/addsuppliers.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-include("db.php");
+include("D:\VS Code\Project\DTB\cdq2\pets-management\Online-Shopping-System-With-Server-main\admin\admin\includes\db.php");
 
 if(isset($_POST['btn_save'])) {
     $first_name = $_POST['first_name'];
