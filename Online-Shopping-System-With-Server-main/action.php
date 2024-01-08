4ddec51dab0647cec2a4afcdcdbd2e82859134d3
Nguyễn Mỹ Duyên <myduyencocsau@gmail.com>
2024-01-08 18:08:38 +0700

    Update action.php

--- Online-Shopping-System-With-Server-main/action.php
+++ Online-Shopping-System-With-Server-main/action.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
-$ip_add = getenv("REMOTE_ADDR");
+$ip_add_1 = $_SERVER['REMOTE_ADDR'];
+$ip_add = (string)$ip_add_1;
 include "db.php"; // file db.php chứa các kết nối vs database
 
 if(isset($_POST["category"])){
@@ -520,6 +521,22 @@ if (isset($_POST["get_seleted_Category"]) || isset($_POST["selectBrand"]) || iss
 				}
 			}
 		}
+//Remove Item From cart
+if (isset($_POST["removeItemFromCart"])) {
+	$remove_id = $_POST["rid"];
+	if (isset($_SESSION["uid"])) {
+		$sql = "DELETE FROM cart WHERE p_id = '$remove_id' AND user_id = '$_SESSION[uid]'";
+	}else{
+		$sql = "DELETE FROM cart WHERE p_id = '$remove_id' AND ip_add = '$ip_add'";
+	}
+	if(sqlsrv_query($con,$sql)){
+		echo "<div class='alert alert-danger'>
+						<a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
+						<b>Product is removed from cart</b>
+				</div>";
+		exit();
+	}
+}
 	
 	
 // Update Item From cart
